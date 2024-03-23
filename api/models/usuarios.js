cfd3f7a4ff9b3027f218fb53d54efbe11b9995cb
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2024-03-23 14:31:42 -0300

    Adiciona relacionamentos entre permissoes, role e usuarios

--- api/models/usuarios.js
+++ api/models/usuarios.js
@@ -1,30 +1,40 @@
-'use strict';
+'use strict'
 const {
-  Model
-} = require('sequelize');
+    Model
+} = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
-  class usuarios extends Model {
+    class usuarios extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+        static associate(models) {
+            usuarios.belongsToMany(models.roles, {
+                through: models.usuarios_roles,
+                as: 'usuarios_das_roles',
+                foreignKey: 'usuario_id'
+            })
+
+            usuarios.belongsToMany(models.permissoes, {
+                through: models.usuarios_permissoes,
+                as: 'usuarios_das_permissoes',
+                foreignKey: 'usuario_id'
+            })
+        }
     }
-  }
-  usuarios.init({
-    nome: DataTypes.STRING,
-    email: DataTypes.STRING,
-    senha: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'usuarios',
-    defaultScope: {
-      attributes: {
-        exclude: ['senha']
-      }
-    }
-  });
-  return usuarios;
-};
+    usuarios.init({
+        nome: DataTypes.STRING,
+        email: DataTypes.STRING,
+        senha: DataTypes.STRING
+    }, {
+        sequelize,
+        modelName: 'usuarios',
+        defaultScope: {
+            attributes: {
+                exclude: ['senha']
+            }
+        }
+    })
+    return usuarios
+}
